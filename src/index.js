f8dc462a29567602e461dfdb82ce9a5e1abc58e8
Anna Sedlar <annasedlar@gmail.com>
2017-01-17 16:02:15 -0500

    removed mason comments

--- src/index.js
+++ src/index.js
@@ -8,12 +8,6 @@ import Entertainment from './entertainment/Entertainment.js'
 import Sports from './sports/Sports.js'
 import Business from './business/Business.js'
 
-<<<<<<< HEAD
-//this is a comment
-=======
-//this is a comment, 2
->>>>>>> 7b31fee157f218c8cb7c80a4b10408c4b34e3a13
-
 ReactDOM.render(
 	<Router history={hashHistory}>
 		<Route path="/" component={App} >
