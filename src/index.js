de6fa676cced61cde797da97894044a73897225c
Anna Sedlar <annasedlar@gmail.com>
2017-01-20 10:45:56 -0500

    sports routing

--- src/index.js
+++ src/index.js
@@ -10,6 +10,9 @@ import Business from './business/Business.js'
 import Global from './global/Global.js'
 import SearchResults from './search/SearchResults.js'
 import Nba from './sports/Nba.js';
+import Nfl from './sports/Nfl.js';
+import Mlb from './sports/Mlb.js';
+import Nhl from './sports/Nhl.js';
 import SportsHome from './sports/SportsHome.js';
 
 
@@ -21,6 +24,9 @@ ReactDOM.render(
 			<Route path="sports" component={Sports} >
 				<IndexRoute component={SportsHome} />
 				<Route path="/nba" component={Nba} />
+				<Route path="/nfl" component={Nfl} />
+				<Route path="/mlb" component={Mlb} />	
+				<Route path="/nhl" component={Nhl} />				
 			</Route>
 			<Route path="business" component={Business} />
 			<Route path="global" component={Global} />
